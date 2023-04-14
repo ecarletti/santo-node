2ac81d59e99b450b35dc92b7f9992c260e90ba92
ecarletti <carlettieze@gmail.com>
2023-04-14 17:29:58 -0300

    fix

--- src/routes/note.routes.js
+++ src/routes/note.routes.js
@@ -4,9 +4,9 @@ const notes = require("../controllers/note.controller");
 const router = express.Router();
 
 router.post("/", notes.create);
-router.get("/", notes.findAll);
-router.get("/:id", notes.findOne);
-router.put("/:id", notes.update);
-router.delete("/:id", notes.delete);
+// router.get("/", notes.findAll);
+// router.get("/:id", notes.findOne);
+// router.put("/:id", notes.update);
+// router.delete("/:id", notes.delete);
 
 module.exports = router;
